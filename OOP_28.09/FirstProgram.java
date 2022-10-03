70edba2e5ed616271b8138a7019643b4a38e0a48
Ivan (John) Kosiakov <92152254+RAYNingTime@users.noreply.github.com>
2022-10-03 23:57:48 +0300

    Create FirstProgram.java

---
+++ OOP_28.09/FirstProgram.java
@@ -0,0 +1,10 @@
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		System.out.print("This is my");
+        System.out.print(System.lineSeparator());
+        System.out.print("first program");
+	}
+}
+
