9eb04c7d43189eb220f265d510e9654b1180d448
Ivan (John) Kosiakov <92152254+RAYNingTime@users.noreply.github.com>
2022-10-03 23:52:37 +0300

    Create Mult_Q2.java

---
+++ OOP_03.10/Mult_Q2.java
@@ -0,0 +1,18 @@
+package Lab1;
+
+/*
+ * This program used to test a second multiple choice exercise.
+ */
+public class Mult_Q2 {
+
+	// Turning off the warnings of unused variables
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		int count; // This name is valid, because it starts with lower case letter
+		double $count; // This name is valid, because it starts with special symbol and contains lower case letters
+		float count27; // This name is valid, because it starts with lower case letter and contains number
+		
+		/* char 67count; */ // This name is invalid, because it starts with number.
+	}
+
+}
