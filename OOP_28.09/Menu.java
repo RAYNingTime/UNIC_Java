06a04ed55ef9d4ebd1bce06e2754570f87872446
Ivan (John) Kosiakov <92152254+RAYNingTime@users.noreply.github.com>
2022-10-05 10:14:52 +0300

    Update Menu.java

--- OOP_28.09/Menu.java
+++ OOP_28.09/Menu.java
@@ -2,12 +2,14 @@ import java.util.Scanner;
 
 public class Menu {
 	public static void main(String[] args) {
+		// Using some default values for the age, name and surname
+		// to avoid an error with straight call for the "3"
 		int option, age = -1;
 		String name = " ", surname = " ";
 		
 		Scanner sc= new Scanner(System.in);
 		do {
-			System.out.print("\tMenu\n1. Enter your name and surname\n2. Enter your age\n"); 
+			System.out.print("\n\tMenu\n1. Enter your name and surname\n2. Enter your age\n"); 
 			System.out.print("3. Prints-out your data\n4. Exit\n\n Select an option:");
 			option = sc.nextInt();
 			
@@ -25,10 +27,12 @@ public class Menu {
 				break;
 			}
 			case 3: {
+				// Catching an errors if user didn't change one of the values
+				// or changed it, but didn't enter any info.
 				if (age < 0 || name == " " || surname == " ")
-					System.out.print("ERROR. You didn't entered a data OR you've entered an invalid value!\n\n");
+					System.out.print("\nERROR. You didn't entered a data OR you've entered an invalid value!");
 				else 
-					System.out.println("Hello, "+ name + " " + surname + ". You're " + age + " years old!\n\n");
+					System.out.println("\nHello, "+ name + " " + surname + ". You're " + age + " years old!");
 					
 				break;
 			}
@@ -36,7 +40,7 @@ public class Menu {
 				// No code needed
 				break;
 			}
-			default: System.out.print("You've entered an invalid number! Try again!"); 
+			default: System.out.print("\nYou've entered an invalid number of the menu! Try again!\n"); 
 			}
 		} while(option != 4);
 		
