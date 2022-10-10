8b46cfa9a2f86839a513db9dc54fc9f4cec8c973
Ivan (John) Kosiakov <92152254+RAYNingTime@users.noreply.github.com>
2022-10-10 23:45:12 +0300

    Create Mult_Q1.java

---
+++ OOP/10.10/Mult_Q1.java
@@ -0,0 +1,27 @@
+public class Mult_Q1 {
+
+	public static void calculate(int age, double size) {
+		//Some code
+	}
+	
+	public static void main(String[] args) {
+		
+		int age = 34;
+		int weight = 180;
+		double height = 5.9;
+		
+		calculate(age,weight);
+		calculate(age,height);
+		calculate(weight,height);
+		//calculate(height,age);		
+		//calculate(45.5,120);
+		calculate(12,120.2);
+		//calculate(age,size);
+		calculate(2,3);
+		//calculate(age);
+		calculate(weight,weight);
+		
+		
+	}
+
+}
