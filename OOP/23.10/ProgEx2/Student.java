94f9852ba18842ed0cfb484356d6d40dc2d2f6a6
Ivan (John) Kosiakov <92152254+RAYNingTime@users.noreply.github.com>
2022-10-24 16:47:06 +0300

    Create Student.java

---
+++ OOP/23.10/ProgEx2/Student.java
@@ -0,0 +1,70 @@
+
+public class Student {
+	private int studentId;
+	private int creditHours;
+	private int studentPoints;
+	private double gradeAverage;
+	
+	public Student() {
+		studentId = 9999;
+		creditHours = 3;
+		studentPoints = 12;
+		gradeAverage = 0;
+	}
+	
+	// This method calculates the average of the grades
+	public void calculateAverage() {
+		gradeAverage = (double)(studentPoints / creditHours);
+	}
+	
+	
+	// ==== Setting the properties of the class ====
+	public void setId(int id) {
+		studentId = id;
+	}
+	
+	public void setHours(int hours) {
+		creditHours = hours;
+	}
+	
+	public void setStudentPoints(int points) {
+		studentPoints = points;
+	}
+	
+	
+	// ==== Showing the properties of the class ====
+	public void showId() {
+		System.out.print("Student ID is:" + studentId);
+	}
+	
+	public void showCreditHours() {
+		System.out.print("Credit Hours are:" + creditHours);
+	}
+	
+	public void showPoints() {
+		System.out.print("Student points are:" + studentPoints);
+	}
+	
+	public void showGradeAverage() {
+		System.out.print("Grade average is:" + gradeAverage);
+	}
+	
+	
+	// ==== Getting the properties of the class ====
+	public int getId() {
+		return studentId;
+	}
+	
+	public int getCreditHours() {
+		return creditHours;
+	}
+	
+	public int getPoints() {
+		return studentPoints;
+	}
+	
+	public double getGradeAverage() {
+		return gradeAverage;
+	}
+	
+}
