22e73fc3fe38046c9a4128eee465991d1b026269
Ivan (John) Kosiakov <92152254+RAYNingTime@users.noreply.github.com>
2022-10-27 09:53:19 +0300

    Create Distance.java

---
+++ OOP/26.10/Distance.java
@@ -0,0 +1,36 @@
+public class Distance {
+	private	double speed;
+	private	int hours;
+		
+	// Constructor
+	public Distance(double userSpeed, int userHours) {
+		setSpeed(userSpeed);
+		setHours(userHours);
+	}
+	
+	// This method sets speed to store it in class
+	private void setSpeed(double userSpeed) {
+		this.speed = userSpeed;
+	}
+	
+	// This method sets hours to store them in class
+	private void setHours(int userHours) {
+		this.hours = userHours;
+	}
+	
+	// This method returns speed that have been stored in class
+	public double getSpeed() {
+		return speed;
+	}
+	
+	// This method returns hours that have been stored in class
+	public int getHours() {
+		return hours;
+	}
+	
+	// This method returns the distance 
+	// traveled in hours at the specified speed
+	public double getDistance() {
+		return speed*hours;
+	}
+}
