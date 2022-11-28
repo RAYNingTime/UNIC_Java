2983c22df0a43135f9110a5057989ef7fa7a14a3
Ivan (John) Kosiakov <92152254+RAYNingTime@users.noreply.github.com>
2022-11-28 23:40:27 +0200

    Create Building.java

---
+++ OOP/28.11/Building/Building.java
@@ -0,0 +1,10 @@
+package Building;
+
+public abstract class Building {
+	public void displayArea() {
+		System.out.println("Area of the building is 3 square km");
+	}
+	
+	public abstract void method1();
+	public abstract void method2();  
+}
