2c31bfed15127cf057698aef19a758296eee6d8d
Ivan (John) Kosiakov <92152254+RAYNingTime@users.noreply.github.com>
2022-11-28 23:40:38 +0200

    Create EuropaBuilding.java

---
+++ OOP/28.11/Building/EuropaBuilding.java
@@ -0,0 +1,13 @@
+package Building;
+
+public class EuropaBuilding extends UniversityBuilding{
+
+	public void method1() {
+		System.out.println("Abstract method 1 in EuropaBuliding class");
+	}
+	
+	public void method2() {
+		System.out.println("Abstract method 2 in EuropaBuliding class");
+	}
+
+}
