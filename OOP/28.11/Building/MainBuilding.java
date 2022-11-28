e980d9d2daa78937c674e20f5eb55ae57f2ee342
Ivan (John) Kosiakov <92152254+RAYNingTime@users.noreply.github.com>
2022-11-28 23:40:47 +0200

    Create MainBuilding.java

---
+++ OOP/28.11/Building/MainBuilding.java
@@ -0,0 +1,20 @@
+package Building;
+
+public class MainBuilding extends UniversityBuilding{
+	public void roomsNum() {
+		System.out.println("Rooms number in Building is 720");
+	}
+
+	public void parksNum() {
+		System.out.println("Parks number in Building is 320");
+	}
+	
+	public void method1() {
+		System.out.println("Abstract method 1 in UniversityBuliding class");
+	}
+	
+	public void method2() {
+		System.out.println("Abstract method 2 in UniversityBuliding class");
+	}
+
+}
