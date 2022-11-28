e0612777c0bb4aab223210699540b8ab114051ce
Ivan (John) Kosiakov <92152254+RAYNingTime@users.noreply.github.com>
2022-11-29 01:06:52 +0200

    Update ShowStudent2.java

--- OOP/23.10/Excercise2/ShowStudent2.java
+++ OOP/23.10/Excercise2/ShowStudent2.java
@@ -1,70 +1,16 @@
 package Excercise2;
 import java.util.Scanner;
 
-public class ShowStudent {
+public class ShowStudent2 {
 	static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		Student StudentClass = new Student(); 
 		
-		int id = enterID();
-		int hours = enterHours();
-		int points = enterPoints();
-		
-		// Initialization check
-		StudentClass.setHours(hours);
-		StudentClass.setId(id);
-		StudentClass.setStudentPoints(points);
-		
 		StudentClass.calculateAverage();
 		
-		// Showing check
-		StudentClass.showId();
-		StudentClass.showPoints();
-		StudentClass.showCreditHours();
-		StudentClass.showGradeAverage();
+		System.out.println("FIRST STUDENT\nThe Student with ID: " + StudentClass.getId() + " has " + StudentClass.getPoints() +
+				" points for " + StudentClass.getCreditHours() + " credit hours." + 
+				"\nHis grade average is " + StudentClass.getGradeAverage());
 	}
-	
-	static int enterID() {
-		int id;
-		
-		do {
-		System.out.print("Enter ID of the Student: ");
-		id = sc.nextInt();
-		
-		if(id < 0)
-			System.out.println("You've entered a negative value! Try again!");
-		} while (id < 0);
-		
-		return id;
-	}
-	
-	static int enterHours() {
-		int hours;
-		
-		do {
-		System.out.print("Enter Credit Hours of Student: ");
-		hours = sc.nextInt();
-		
-		if(hours < 0)
-			System.out.println("You've entered a negative value! Try again!");
-		} while (hours < 0);
-
-		return hours;
-	}
-	
-	static int enterPoints() {
-		int points;
-		
-		do {
-		System.out.print("Enter Points of Student: ");
-		points = sc.nextInt();
-		
-		if(points < 0)
-			System.out.println("You've entered a negative value! Try again!");
-		} while (points < 0);
-
-		return points;
-	}
-
 }
