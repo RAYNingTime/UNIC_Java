016eb50a2280763545532b62df4ad10099df916c
Ivan (John) Kosiakov <92152254+RAYNingTime@users.noreply.github.com>
2022-11-29 00:49:20 +0200

    Create Root.java

---
+++ OOP/07.11/Root/Root.java
@@ -0,0 +1,12 @@
+package Root;
+
+public class Root {
+	Component1 obj1 = new Component1();
+	Component2 obj2 = new Component2();
+	Component3 obj3 = new Component3();
+	
+	public Root() {
+		System.out.println("This is Root constructor");
+	}
+
+}
