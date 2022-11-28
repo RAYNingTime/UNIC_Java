a2e31621d9ca96ca7db1af9c1909f57015983e2f
Ivan (John) Kosiakov <92152254+RAYNingTime@users.noreply.github.com>
2022-11-29 00:50:58 +0200

    Create FinalDerivedClass.java

---
+++ OOP/07.11/Tank/FinalDerivedClass.java
@@ -0,0 +1,9 @@
+package Tank;
+
+public class final void FinalDerivedClass extends FinalClass{
+	public FinalDerivedClass() {
+		System.out.println("This is Final Derived Class constructor");
+	}
+	
+
+}
