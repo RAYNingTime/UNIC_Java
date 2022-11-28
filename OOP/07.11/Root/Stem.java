2924108779a971fba1cb7a6dea9978bf74b4429d
Ivan (John) Kosiakov <92152254+RAYNingTime@users.noreply.github.com>
2022-11-29 00:49:27 +0200

    Create Stem.java

---
+++ OOP/07.11/Root/Stem.java
@@ -0,0 +1,12 @@
+package Root;
+
+public class Stem extends Root{
+	Component1 obj1 = new Component1();
+	Component2 obj2 = new Component2();
+	Component3 obj3 = new Component3();
+	
+	public Stem() {
+		System.out.println("This is Stem constructor");
+	}
+
+}
