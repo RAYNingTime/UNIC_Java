a19b6d1d9f0e7e89df9ea1121ba3b974d8c72aa6
Ivan (John) Kosiakov <92152254+RAYNingTime@users.noreply.github.com>
2022-11-28 23:39:43 +0200

    Create Insurance.java

---
+++ OOP/28.11/Insurance/Insurance.java
@@ -0,0 +1,24 @@
+package Insurance;
+
+public abstract class Insurance {
+	  protected String type;
+	  protected double cost;
+	  
+	  public Insurance() {
+	    
+	  }
+	  
+	  public Insurance(String lh) {
+	    type = lh;
+	  }
+	  
+	  public String getType() {
+	    return type;
+	  }
+	  public double getCost() {
+	    return  cost;
+	  }
+	  public abstract void setCost();
+	  public abstract void display();
+
+	}
