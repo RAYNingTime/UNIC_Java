d2150897c623570632bb45998b18fea524c87582
Ivan (John) Kosiakov <92152254+RAYNingTime@users.noreply.github.com>
2022-11-29 00:31:22 +0200

    Create Demo.java

---
+++ OOP/28.11/InterfaceEx4/Demo.java
@@ -0,0 +1,19 @@
+package InterfaceEx4;
+
+public class Demo {
+	public static void main(String[] args) {
+		Test test = new Test();
+		
+		test.method1(test);
+		System.out.println();
+		
+		test.method2(test);
+		System.out.println();
+		
+		test.method3(test);
+		System.out.println();
+		
+		test.method4(test);
+	}
+
+}
