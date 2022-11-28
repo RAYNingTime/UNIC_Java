b81b5040fe2bc1d9f4d25f04ff376b5ebff94de2
Ivan (John) Kosiakov <92152254+RAYNingTime@users.noreply.github.com>
2022-11-28 23:41:06 +0200

    Create UniversityBuilding.java

---
+++ OOP/28.11/Building/UniversityBuilding.java
@@ -0,0 +1,20 @@
+package Building; 
+
+public abstract class UniversityBuilding extends Building{
+	public void roomsNum() {
+		System.out.println("Rooms number in Building is 720");
+	}
+
+	public void parksNum() {
+		System.out.println("Parks number in Building is 320");
+	}
+	
+	public void method1() {
+		System.out.println("Abstract method 1 in UniversityBuliding class");
+	}
+	
+	public void method2() {
+		System.out.println("Abstract method 2 in UniversityBuliding class");
+	}
+  
+}
