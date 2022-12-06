d06383a7ebf92afaa1543ddf12fe08043f32cbaa
Ivan (John) Kosiakov <92152254+RAYNingTime@users.noreply.github.com>
2022-12-06 09:40:46 +0200

    Create Concrete.java

---
+++ OOP/05.12/AandB/Concrete.java
@@ -0,0 +1,13 @@
+package AandB;
+
+public class Concrete extends B {
+
+	public void abs1() {
+		System.out.println("Method 1 of abstract class");
+	}
+
+	public void abs2() {
+		System.out.println("Method 2 of abstract class");
+	}
+
+}
