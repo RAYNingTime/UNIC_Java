9eea826f76842664646b166acd19923ecd262e2c
Ivan (John) Kosiakov <92152254+RAYNingTime@users.noreply.github.com>
2022-12-06 09:43:27 +0200

    Create Library.java

---
+++ OOP/05.12/Library/Library.java
@@ -0,0 +1,10 @@
+package Library;
+
+interface Library {
+	String name = "University of Nicosia Library";
+	double capacity = 100000;
+	
+	String getName();
+	double getCapacity();
+	double getArea();
+}
